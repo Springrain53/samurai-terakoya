8e347cf4f3178de52e9d6f3f0874e2e4d32cf13c
Springrain53 <154506565+Springrain53@users.noreply.github.com>
2023-12-27 22:22:08 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,10 @@
+<body>
+    <html>
+        <p>
+            <?php
+                  $food_data = ['name' => 'onion', 'price' => 200, 'weight' => '160'];
+                  print_r($food_data);
+            ?>
+        </p>
+    </html>
+</body>
